b337a37eb7aa932f02b866176356bb2ce2747dac
Dinesh Udayan <dinesh1599@gmail.com>
2025-02-27 23:27:20 -0600

    server.js setup complete

--- BE/server.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const axios = require("axios");
-const cors = require("cors");
-const validator = require("validator");
-require("dotenv").config();
---
+++ BE/src/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
+
+
+//All Routes are mentioned here
+const analyzeRoute = require('./routes/analyzeRoute')
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use (cors());
+app.use (bodyParser.json());
+
+//use Routes
+app.use("/analyze", analyzeRoute)
+app.listen(PORT, ()=> console.log(`Server Running on ${PORT}`))
+
