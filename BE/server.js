bb47456058dc2d10c06eb32959e469b7250f22d0
Dinesh Udayan <dinesh1599@gmail.com>
2025-02-27 23:13:20 -0600

    First commit

---
+++ BE/server.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+const cors = require("cors");
+const validator = require("validator");
+require("dotenv").config();
