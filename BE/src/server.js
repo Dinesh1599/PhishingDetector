390ec28d14782efb869228cdae0329c850765c3f
Dinesh Udayan <dinesh1599@gmail.com>
2025-02-27 23:30:14 -0600

    analyzeRouter initial setup and updated PORT

--- BE/src/server.js
+++ BE/src/server.js
@@ -8,7 +8,7 @@ require("dotenv").config();
 const analyzeRoute = require('./routes/analyzeRoute')
 
 const app = express();
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use (cors());
 app.use (bodyParser.json());
