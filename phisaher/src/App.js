f254effd2e4b6df9f415317407bdca03fff3441d
Dinesh Udayan <dinesh1599@gmail.com>
2025-02-28 15:56:51 -0600

    Backend connection esatblished

--- phisaher/src/App.js
+++ phisaher/src/App.js
@@ -1,18 +1,40 @@
 import { useState } from "react";
+import axios from "axios";
 import './App.css';
 
 function App() {
   const [input, setInput] = useState("")
+  const [result, setResult] = useState(null)
+
+  const checkPhis = async()=>{
+       try{
+        const response = await axios.post("http://localhost:5000/analyze",{emailContent:input})
+        setResult(response)
+
+       }catch(error){
+        console.error("Error analyzing email:", error);
+       }
+
+       if(result == null)
+        alert("Missing Input Text")
+  }
+
+  const reset = ()=>{
+    setInput("");
+    setResult(null);
+  }
+
+
   return (
     <div className = "page_main">
       <div className = "TheScreen">
         <div className = 'Header'>
-          <h1>Phishing Tester</h1>
+          <h1>Phishing Probability Tester</h1>
         </div>
         <div className ='contentBody'>
           <div className = 'theStartBtn'>
-            <button className="startBtn">Submit</button>
-            <button className="resetBtn">Reset</button>
+            <button className="startBtn" onClick={checkPhis}>Submit</button>
+            <button className="resetBtn" onClick={reset}>Reset</button>
           </div>
           <div className = 'bodySplit'>
             <div className = 'split1'>
